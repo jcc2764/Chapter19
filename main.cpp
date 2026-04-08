2875342a74808d0255ac2656302d38eb9c4a18a0
Josh Clark <joscclark14@hotmail.com>
2026-04-07 21:09:36 -0400

    Added userID to name

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-//Name: Joshua Clark
-//Date 04.07.2026
-//Purpose: Store customer information and their rented DVDs using a binary tree
+// Name: Joshua Clark (jcc2764)
+// Date 04.07.2026
+// Purpose: Store customer information and their rented DVDs using a binary tree
 
 #include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
     // Write your main here
     return 0;
 }
