ca5ac2b3ea3fa34eaa65ed54766a3e09d5478bd5
kjecobra <53786356+kjecobra@users.noreply.github.com>
2026-04-16 10:30:34 -0400

    Refactor customerType class and add extended info

--- customer.h
+++ customer.h
@@ -1,3 +1,9 @@
+// ===============================
+// ASSIGNMENT 19-11  DVD RENTAL STORE DATABASE
+// AUTHOR: Joseph Evans
+// DATE: 4/16/2026
+// ===============================
+
 #ifndef H_customerType
 #define H_customerType
 
@@ -5,57 +11,94 @@
 #include "personType.h"
 #include <string>
 #include "binarySearchTree.h"
-  
+
 using namespace std;
 
+/*
+    customerType
+    ------------
+    Represents a single customer in the DVD store system.
+    Inherits first/last name from personType and adds:
+
+        - Account number
+        - Address
+        - Phone number
+        - Member-since date
+        - A binary search tree of rented DVDs
+
+    The rented DVDs are stored in a BST of strings so that
+    insertion, deletion, and lookup are efficient.
+*/
 class customerType: public personType
-{ 
-   friend ostream& operator<<(ostream&,  customerType&);
-	  //overload stream insertion operator
+{
+    // Allows formatted printing using: cout << customerObject;
+    friend ostream& operator<<(ostream&, customerType&);
+
 public:
-	void print();
-	  //Output account number, first name, last name, and 
-	  //number of rentals, in the form:
-	  //acctNo firstName lastName noOfRentals
-	void setCustInfo(string first, string last, int acctNo);
-	  //set firstName, lastName, and account number 
-	  //according to the parameters
-	  //firstName = first; lastName = last; 
-	void rentDVD(string);
-	  //This function rents a DVD to the customer
-	void returnDVD(string);
-	  //This function renturns a DVD to the customer
+    // Prints: acctNo firstName lastName noOfRentals
+    void print();
 
-	int getNoOfRentals();
-	  //This function returns the number of
-	  //DVDs rented by the customer.
-	int getAcctNo();
-	  //This function returns the account number of
-	  //the customer.
+    // Sets first name, last name, and account number
+    void setCustInfo(string first, string last, int acctNo);
 
-	void printRentedDVD();
+    // Adds a DVD title to the customer's rental BST
+    void rentDVD(string);
 
-	customerType(string first, string last, int acctNo);
-	  //constructor with parameters
-	  //set firstName and lastName according to the parameters
-	  //firstName = first; lastName = last;
-  	customerType(); 
-  	 //default constructor;
-  	 //intialize firstName and lastName, and custAcctNo 
-  	 //Postcondition: firstName = ""; lastName = "";
-	 //		  custAcctNo = 0;
+    // Removes a DVD title from the customer's rental BST
+    void returnDVD(string);
 
-		//overload operators for comparisons 
-	bool operator==(const customerType&) const;
-	bool operator!=(const customerType&) const;
-	bool operator<=(const customerType&) const;
-	bool operator<(const customerType&) const;
-	bool operator>=(const customerType&) const;
-	bool operator>(const customerType&) const;
-     
+    // Returns how many DVDs the customer currently has rented
+    int getNoOfRentals();
+
+    // Returns the customer's account number
+    int getAcctNo();
+
+    // Prints all DVDs rented by this customer (inorder traversal)
+    void printRentedDVD();
+
+    // Constructor with parameters
+    customerType(string first, string last, int acctNo);
+
+    // Default constructor
+    customerType();
+
+    // Comparison operators (compare ONLY by account number)
+    bool operator==(const customerType&) const;
+    bool operator!=(const customerType&) const;
+    bool operator<=(const customerType&) const;
+    bool operator<(const customerType&) const;
+    bool operator>=(const customerType&) const;
+    bool operator>(const customerType&) const;
+
+    // Extended info setters
+    void setAddress(string addr);
+    void setPhone(string ph);
+    void setMemberSince(string ms);
+
+    // Extended info getters
+    string getAddress() const;
+    string getPhone() const;
+    string getMemberSince() const;
+
+    /*
+        Prints full customer information in a readable format:
+            - ID
+            - Name
+            - Address
+            - Phone (formatted)
+            - Member since
+            - List of rented DVDs
+    */
+    void printFullInfo();
+
 private:
-	int custAcctNo; // store account number
-	bSearchTreeType<string> rentedDVDTree;
+    int custAcctNo;                        // Unique account number
+    bSearchTreeType<string> rentedDVDTree; // BST storing rented DVD titles
+
+    // Extended customer information
+    string address;
+    string phone;
+    string memberSince;
 };
 
 #endif
